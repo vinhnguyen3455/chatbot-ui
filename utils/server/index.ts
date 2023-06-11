8c93396cfbec59c180e0bc0442a0af1998d9a5b1
Vinh Nguyen <vinhnguyen3455@gmail.com>
2023-06-10 19:03:59 -0700

    Fix response not rendered

--- utils/server/index.ts
+++ utils/server/index.ts
@@ -9,8 +9,19 @@ import {
   createParser,
 } from 'eventsource-parser';
 
-export const OpenAIError = Error;
+export class OpenAIError extends Error {
+  type: string;
+  param: string;
+  code: string;
 
+  constructor(message: string, type: string, param: string, code: string) {
+    super(message);
+    this.name = 'OpenAIError';
+    this.type = type;
+    this.param = param;
+    this.code = code;
+  }
+}
 
 export const OpenAIStream = async (
   model: OpenAIModel,
@@ -30,29 +41,52 @@ export const OpenAIStream = async (
     }),
   });
 
-  if (!res.ok) {
-    throw new OpenAIError(`HTTP error! Status: ${res.status}`);
+  const encoder = new TextEncoder();
+  const decoder = new TextDecoder();
+
+  if (res.status !== 200) {
+    const result = await res.json();
+    if (result.error) {
+      throw new OpenAIError(
+        result.error.message,
+        result.error.type,
+        result.error.param,
+        result.error.code,
+      );
+    } else {
+      throw new Error(
+        `OpenAI API returned an error: ${
+          decoder.decode(result?.value) || result.statusText
+        }`,
+      );
+    }
   }
 
   const stream = new ReadableStream({
     async start(controller) {
-      if (res.body === null) {
-        // Handle the case when res.body is null
-        controller.close();
-        return;
-      }
+      const onParse = (event: ParsedEvent | ReconnectInterval) => {
+        if (event.type === 'event') {
+          const data = event.data;
 
-      const reader = res.body.getReader();
-
-      while (true) {
-        const { done, value } = await reader.read();
-
-        if (done) {
-          controller.close();
-          break;
+          try {
+            const json = JSON.parse(data);
+            if (json.choices[0].finish_reason != null) {
+              controller.close();
+              return;
+            }
+            const text = json.choices[0].delta.content;
+            const queue = encoder.encode(text);
+            controller.enqueue(queue);
+          } catch (e) {
+            controller.error(e);
+          }
         }
+      };
 
-        controller.enqueue(value);
+      const parser = createParser(onParse);
+
+      for await (const chunk of res.body as any) {
+        parser.feed(decoder.decode(chunk));
       }
     },
   });
